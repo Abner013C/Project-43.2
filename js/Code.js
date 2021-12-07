2d3a99e538e824fcbdcc5aba32ee0064fdaf52da
Abner Carbonell <89807868+Abner013C@users.noreply.github.com>
2021-12-07 16:54:09 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,15 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    text("A T E D A S B A", 240,350);
+    text("C U T N I F O N", 700,130);
+    fill("lightblue");
+    text("Hint: Always changing, not constant.", 100,70);
+    text("Hint: Stores all information.", 240,370)
+    text("Hint: Performs a particular task.", 700,150)
+}
